6d588b029fc88c5d9b908f9431421b4f16db7063
Alaa Shaaban <alaa.shaaban943@gmail.com>
2025-07-31 18:27:29 +0300

    init homepage and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <div>
-    worldwise
-    </div>
-  );
-}
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={ <Home/>}/>
+    </Routes>
+  </BrowserRouter>
+)}
 export default App;
