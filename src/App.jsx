3cc7437bf6ae44dc9d729689457e9bed0141271b
Alaa Shaaban <alaa.shaaban943@gmail.com>
2025-08-01 15:56:10 +0300

    login & pricing

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,22 @@ import Home from './pages/Home';
 import AppLayout from './pages/AppLayout';
 import './App.css';
 import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import Login from './pages/Login';
+import { AuthProvider } from './contexts/FakeAuthContext';
 
 function App() {
   return (
+  <AuthProvider>
   <BrowserRouter className="app">
     <Routes>
       <Route path='/' element={ <Home/>}/>
       <Route path='/App' element={ <AppLayout/> }/>
-      <Route path='/pricing' element={ <div>Pricing Page</div> }/>
+      <Route path='/pricing' element={<Pricing/>}/>
       <Route path='/product' element={ <Product/> }/>
-      <Route path='/login' element={ <div>Login Page</div> }/>
+      <Route path='/login' element={<Login/> }/>
     </Routes>
   </BrowserRouter>
+  </AuthProvider>
 )}
 export default App;
